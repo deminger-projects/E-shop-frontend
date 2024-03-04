7b44893dec1f47da288f127625f0ececf5d1d631
pikosoZBrna <dominikjan1@gmail.com>
2024-03-04 23:53:38 +0100

    react

--- src/apis/cart/add_to_cart.ts
+++ src/apis/cart/add_to_cart.ts
@@ -7,17 +7,22 @@ export default async function add_to_cart(product: Product, size: ProductSize){
     form_data.append("product", JSON.stringify(product))
     form_data.append("selected_size", JSON.stringify(size))
 
+    console.log(process.env.REACT_APP_SECRET_SERVER_URL)
+
     try{
         const responce = await fetch(process.env.REACT_APP_SECRET_SERVER_URL + '/add_to_cart', {
             method: 'POST',
             body: form_data
         })
 
+        console.log(responce)
+
         return [await responce.json(), undefined]
 
     } catch(err) {
         console.log("🚀 ~ file: add_record.ts:40 ~ add_record ~ err:", err)
-        
+        console.log("🚀 ~ file: add_record.ts:40 ~ add_record ~ err:", err.msg)
+
         return [undefined, err]
     }
 }
