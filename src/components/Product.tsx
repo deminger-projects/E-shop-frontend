3f3df3db5e4fa18801266c02b19b808eb2a97219
pikosoZBrna <dominikjan1@gmail.com>
2024-09-26 14:50:36 +0200

    react

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -11,7 +11,9 @@ export default function Product_comp(props: {item: Product}){
             <div key={product.products[0].id.toString()} className="grid-item">
                 <Link to={"/item-info/" + product.products[0].id} state={{product_data: product}}>
                     <p>{product.products[0].product_name}</p>
-                    <img alt={product.products[0].product_name} className="images" src={process.env.REACT_APP_SECRET_SERVER_URL + "/images/products/" + product.products[0].id + "/" + product.products[0].url} width={"100px"} height={"100px"}></img>
+                    {//<img alt={product.products[0].product_name} className="images" src={process.env.REACT_APP_SECRET_SERVER_URL + "/images/products/" + product.products[0].id + "/" + product.products[0].url} width={"100px"} height={"100px"}></img>
+                    }
+                    <img alt={product.products[0].product_name} className="images" src={"/image_storage" + "/images/products/" + product.products[0].id + "/" + product.products[0].url} width={"100px"} height={"100px"}></img>
                     <p>{"€" + product.products[0].price}</p>
                 </Link>
                                 
